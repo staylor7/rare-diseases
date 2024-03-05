5eefb045c344362bd4bea1359b5b154776d99bea
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2024-03-04 23:32:56 -0600

    Fix build target error and remove unnecessary path

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { draw } from "./raredisease";
 import { parseCsv } from "./utils";
 
-const csvPath = import.meta.env.BASE_URL + "seq.d3.csv";
+const csvPath = "seq.d3.csv";
 const container = document.getElementById("catalog");
 const data = await parseCsv(csvPath);
 
