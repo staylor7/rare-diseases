62e0ea4cf8f5d991881495b8c2414b97ad932bf9
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2024-03-16 16:39:35 -0500

    Add rudimentary Cloudflare build configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
-    base:
-      mode === "development"
-        ? "http://localhost:5173/rare-diseases" // Development, local
-        : // : "https://stephenandrewtaylor.net/rare-test", // Production, staging
-          "http://localhost:4173/rare-test", // Production, local
     build: {
       target: "esnext",
-      outDir: "rare-test",
       sourcemap: true, // false for "true" production (non-staging)
     },
   };
