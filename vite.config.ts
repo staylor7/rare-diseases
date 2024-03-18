d19416d9b2f18da81f2173d4b0a19a6187e787ec
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2024-03-18 00:50:29 -0500

    Add local HTTPS support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
     build: {
       target: "esnext",
       sourcemap: true, // false for "true" production (non-staging)
     },
+    plugins: mode === "development" ? [basicSsl()] : [],
   };
 });
